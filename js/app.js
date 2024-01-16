a1bafe698fada4e3d5c8c05d8f60185e1831ec8a
NghiMartin <soobinnghi@gmail.com>
2024-01-16 17:04:18 +0700

    updateResposive

--- js/app.js
+++ js/app.js
@@ -1,10 +1,45 @@
-const logo_items = document.querySelectorAll('.logo_item');
-logo_items.forEach(logo_item => {
-    logo_item.addEventListener('mouseover', function() {
-        logo_item.style.padding = '2rem';
+document.addEventListener("DOMContentLoaded", function () {
+
+  const counters = document.querySelectorAll(".counter");
+
+  window.addEventListener("scroll", function () {
+    
+      counters.forEach((counter) => {
+          const rect = counter.getBoundingClientRect(); // trả về object có kích thước và vị trí của phần tử trong trang web.
+          if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+              // Nếu counter hiển thị trên màn hình (ít nhất một phần)
+
+              animateCounter(counter, counter.id);
+               
+          }
       });
+      // Effect nhảy số
+      function animateCounter(counter, targetValue) {
+        counter.textContent = 0;
+        let currentValue = 0;
+        const increment = Math.ceil(targetValue / 100); // Chia targetValue thành 100 phần
+        const animationSpeed = 10; // Tốc độ animation
   
-      logo_item.addEventListener('mouseout', function() {
-        logo_item.style.padding = '0rem';
-      });
-})
+        const updateCounter = () => {
+            currentValue += increment;
+            if (currentValue >= targetValue) {
+                currentValue = targetValue;
+                clearInterval(interval);
+            }
+            counter.textContent = currentValue;
+        };   
+        const interval = setInterval(updateCounter, animationSpeed);
+        counter.classList.add("show"); // Hiển thị counter bằng cách thêm class "show"
+    }
+  
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navMenu = document.querySelector('.header_nav');
+
+  hamburgerMenu.addEventListener('click', function () {
+      navMenu.classList.toggle('show');
+  });
+});
